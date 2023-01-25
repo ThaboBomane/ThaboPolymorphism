8e523c75de433c75eb6076fd0ca98148fb4115c2
ThaboBomane <thabobomane2@gmail.com>
2023-01-25 11:49:09 +0200

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaboPolymorphism;
+class Animal 
+{
+    public virtual void animalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+
+class Pig : Animal  
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The pig says: wee wee");
+    }
+}
+
+class Dog : Animal  
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
+}
+
